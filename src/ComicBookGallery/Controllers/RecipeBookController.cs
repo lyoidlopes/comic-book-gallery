f77cc27777edb48a4c565cbc06d5780a5136e211
Lyoid Lopes <lyoid_lopes@yahoo.com>
2017-06-11 16:22:09 -0400

    Updated recipe Book Detail View

--- src/ComicBookGallery/Controllers/RecipeBookController.cs
+++ src/ComicBookGallery/Controllers/RecipeBookController.cs
@@ -10,6 +10,19 @@ namespace RecipeBookGallery.Controllers
     {
         public ActionResult Detail() 
         {
+
+            ViewBag.RecipeTitle = "How to Make Ciken Biryani";
+            ViewBag.RecipieId = 101;
+            ViewBag.Description = "<p>How to make Chicken Biryani - Chunks of chicken, flavourful spices and long grain basmati rice layered in a handi, sealed and cooked to perfection.</p>";
+            ViewBag.RecipeInfo = new string[]
+                {
+            "Chef: Lyoid Lopes",
+            "Country: India",
+            "Course: Rice",
+            "Cuisine: Hyderabadi",
+            "Main Ingredients: Boneless Chicken, Basmati Rice"
+                };
+
             return View();
             
         }
